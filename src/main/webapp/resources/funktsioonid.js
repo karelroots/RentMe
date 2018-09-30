dbd257ecddc2bd19c06ae63c74624eb27a67be5b
karelroots <43067621+karelroots@users.noreply.github.com>
2018-09-30 14:03:43 +0300

    Update funktsioonid.js
    CDN failide test: kui CDN serverile ligipääs puudub kasutatakse kohalikku faili

--- src/main/webapp/resources/funktsioonid.js
+++ src/main/webapp/resources/funktsioonid.js
@@ -3,3 +3,10 @@ function submitRefresh() {
         //location.reload(true);
         setTimeout(function(){window.location.reload();},10);
 }
+
+function fallback() {
+        if (! $.fn.modal) {
+                document.write('<script src="/resources/bootstrap.min.js"></script>');
+                document.write('<link rel="stylesheet" href="/resources/bootstrap.min.css">');
+        }
+}
