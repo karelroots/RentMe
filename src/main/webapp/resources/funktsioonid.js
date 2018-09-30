55869cf4d09ea2992ba7afaeb2b5c50b31738f23
karelroots <43067621+karelroots@users.noreply.github.com>
2018-09-30 14:11:03 +0300

    Võte: CDN failide test

--- src/main/webapp/resources/funktsioonid.js
+++ src/main/webapp/resources/funktsioonid.js
@@ -1,10 +1,10 @@
-function submitRefresh() {
+function submitRefresh() { // lehekülje refresh pärast mingit tegevust
 
         //location.reload(true);
         setTimeout(function(){window.location.reload();},10);
 }
 
-function fallback() {
+function fallback() { // kasutame kohalikke resursse kui CDN failid pole kättesaadavad
         if (! $.fn.modal) {
                 document.write('<script src="/resources/bootstrap.min.js"></script>');
                 document.write('<link rel="stylesheet" href="/resources/bootstrap.min.css">');
