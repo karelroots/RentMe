0e3bbe30f2fda4dfd7b478c8b746fc96d60471b1
Karel <karelroots@gmail.com>
2018-10-01 09:28:03 +0300

    Code cleanup for release

--- src/main/java/com/project/rent/data.sql
+++
@@ -1,3 +0,0 @@
-/* Andmebaasi andmete esitamine, salvestamine, agregeeritud andmete esitamine */
-INSERT INTO role VALUES (1, 'ADMIN');
-INSERT INTO role VALUES (2, 'KASUTAJA');
--- src/main/java/com/project/rent/exception/EmailNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.project.rent.exception;
-
-public class EmailNotFoundException extends RuntimeException {
-    private String email;
-
-    public EmailNotFoundException(String email) {
-        this.email = email;
-    }
-}
