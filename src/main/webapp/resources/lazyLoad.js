f5c0ca574884579f6cba4664883744ce014c9955
Karel <karelroots@gmail.com>
2018-10-11 22:45:17 +0300

    Profiil

--- src/main/webapp/resources/lazyLoad.js
+++ src/main/webapp/resources/lazyLoad.js
@@ -1,9 +1,11 @@
 $(function(){
-    $('img.avatar').each(function(){
+    $('img').each(function(){
         var this_image = this;
         var src = $(this_image).attr('src');
-        var lsrc = $(this_image).attr('data-src');
+        var lsrc = $(this_image).attr('onloadeddata');
+
         if(lsrc.length > 0){
+            console.log("töötab")
             var img = new Image();
             $(img).on("load", function() {
                 this_image.src = this.src;
