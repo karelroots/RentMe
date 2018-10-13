c56f4e518c2a53c258aacb7f9ed19a85b8458eb6
Karel <karelroots@gmail.com>
2018-10-13 10:41:13 +0300

    Fixed logger & changed stats header

--- src/main/java/com/project/rent/service/LoggingService.java
+++ src/main/java/com/project/rent/service/LoggingService.java
@@ -1,12 +1,9 @@
 package com.project.rent.service;
 
 import com.project.rent.model.UserLog;
-import com.project.rent.repository.UserLogRepository;
-import com.project.rent.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.uadetector.ReadableUserAgent;
 import net.sf.uadetector.UserAgentStringParser;
-import net.sf.uadetector.VersionNumber;
 import net.sf.uadetector.service.UADetectorServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.trace.http.HttpTrace;
@@ -20,40 +17,42 @@ import org.springframework.stereotype.Repository;
 public class LoggingService extends InMemoryHttpTraceRepository {
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @Autowired
-    UserLogRepository userLogRepository;
-
+    StatsService statsService;
 
     @Override
     public void add(HttpTrace trace) {
-        super.add(trace);
+        //super.add(trace);
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        HttpTrace.Request rq = trace.getRequest();
+        if(auth != null) { // tegemist on autoriseeritud kasutajaga
 
-        String page = rq.getUri().getPath();
-        String user = auth.getName();
+            HttpTrace.Request rq = trace.getRequest();
 
-        if (user != null && !page.contains(".")) {
-            UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
-            UserLog userLog = new UserLog();
+            String page = rq.getUri().getPath();
+            String user = auth.getName();
 
-            ReadableUserAgent agent = parser.parse(rq.getHeaders().get("user-agent").get(0));
-            String name = agent.getName();
-            VersionNumber version = agent.getVersionNumber();
-            String osName = agent.getOperatingSystem().getName();
-            String osVersion = agent.getOperatingSystem().getVersionNumber().toVersionString();
+            if (!page.contains(".")) { // laetud leht on html
+                UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser(); // User Agent'i parsimiseks
+                UserLog userLog = new UserLog();
 
-            userLog.setUserId(userRepository.findByEmail(user).getId()); // leiame sellise e-mailiga kasutaja id ja loggime
-            userLog.setOs(osName+" "+osVersion); // loggime kasutaja operatsiooni süsteemi
-            userLog.setLandingPage(rq.getUri().getPath()); // loggime landing page'i
-            userLog.setBrowser(name + "/" + version.toVersionString()); // loggime kasutaja brauseri
+                ReadableUserAgent agent = parser.parse(rq.getHeaders().get("user-agent").get(0));
+                String browserName = agent.getName();
+                String browserVersion = agent.getVersionNumber().toVersionString();
+                String osName = agent.getOperatingSystem().getName();
+                String osVersion = agent.getOperatingSystem().getVersionNumber().toVersionString();
 
-            userLogRepository.save(userLog);
+                userLog.setUserId(userService.findUserByEmail(user).getId()); // leiame sellise e-mailiga kasutaja id ja loggime
+                userLog.setOs(osName + " " + osVersion); // loggime kasutaja operatsiooni süsteemi
+                userLog.setLandingPage(rq.getUri().getPath()); // loggime landing page'i
+                userLog.setBrowser(browserName + "/" + browserVersion); // loggime kasutaja brauseri
 
+                statsService.saveLog(userLog);
+
+            }
         }
     }
 }
